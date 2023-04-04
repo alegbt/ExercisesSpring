60ccbe712e6503adf2a86d486f4debf1226a1ec9
ale <alessandrogbt94@gmail.com>
2023-04-04 18:06:13 +0200

    email1 fatto

---
+++ 23_email1/email-1/src/main/java/co/develhope/email1/emails/services/EmailService.java
@@ -0,0 +1,27 @@
+package co.develhope.email1.emails.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+
+    public void sendTo(String email, String title, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject(title);
+        message.setFrom(sender);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
